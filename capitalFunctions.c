407a46c77ee6b1f93dacebc3c93a0f916c20c82f
elolson93 <elolson93@gmail.com>
2016-09-13 22:13:08 -0400

    Added local and remote logic to the tcpd file

--- capitalFunctions.c
+++ capitalFunctions.c
@@ -13,6 +13,7 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #define LOCALPORT 9999
+#define LOCALADDRESS "127.0.0.1"
 
 /* Prototypes for each function */ 
 ssize_t SEND(int socket, const void* buffer, size_t length, int flags);
@@ -24,14 +25,18 @@ int CONNECT(int sockfd, const struct sockaddr *addr,
 int SOCKET(int domain, int type, int protocol);
 
 
+//define address of local socket to pass data to 
+struct sockaddr_in daemon_addr;
+daemon_addr.sin_family = AF_INET;
+daemon_addr.sin_port = LOCALPORT;
+daemon_addr.sin_addr.s_addr = inet_addr(LOCALADDRESS);
 
 
 int ACCEPT(int sock, struct sockaddr *address, socklen_t *address_len) {
 	return 0;
 }
 
-int CONNECT(int sockfd, const struct sockaddr *addr,
-                   socklen_t addrlen) {
+int CONNECT(int sockfd, const struct sockaddr *addr, socklen_t addrlen) {
 	return 0;
 }
 
@@ -44,22 +49,21 @@ int BIND(int socket, struct sockaddr *my_addr, socklen_t addrlen) {
 }
 
 ssize_t SEND(int socket, const void* buffer, size_t length, int flags) {
-	//define address of local socket to pass data to 
-	struct sockaddr_in name;
-	name.sin_family = AF_INET;
-        name.sin_port = LOCALPORT;
-        name.sin_addr.s_addr = inet_addr("127.0.0.1");
-	
 	//pass data to local socket in tcpd
-	return sendto(socket, buffer, length, 0, (struct sockaddr *)&name, sizeof(name));
+	return sendto(socket, buffer, length, flags, 
+		(struct sockaddr *)&daemon_addr, sizeof(daemon_addr));
 }
 
 ssize_t RECV(int socket, void* buffer, size_t length, int flags) {
+	//send packet to the local tcpd to let it know the server is waiting
+	//buffer waitingMsg
+	sendto(socket, waitingMsg, sizeof(waitingMsg), 0, 
+		(struct sockaddr *)&daemon_addr, sizeof(daemon_addr));
 
-	socklen_t * size;
-	struct sockaddr_in name;
+	//socklen_t * size;
+	//struct sockaddr_in name;
 
 	//recieve from local socket in tcpd
-	return recvfrom(socket, buffer, length, 0, (struct sockaddr*)&name, size);
+	return recvfrom(socket, buffer, length, flags, NULL, NULL);
 }
 
--- tcpd.c
+++ tcpd.c
@@ -4,9 +4,12 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/select.h>
 #include <netinet/in.h>
 #define MSS 1000
 #define LOCALPORT 9999
+#define LOCALADDRESS "127.0.0.1"
+
 
 main(void)
 {
@@ -54,10 +57,73 @@ main(void)
 	//set ip to localhost
         remote_name.sin_addr.s_addr = remote_ip;
 
-        if(bind(local_sock, (struct sockaddr *)&remote_name, sizeof(remote_name)) < 0) {
+        if(bind(remote_sock, (struct sockaddr *)&remote_name, sizeof(remote_name)) < 0) {
 		perror("getting remote socket name");
 		exit(2);
     	}
 
+
+    //set for holding remote and local sockets to listen to
+    fd_set readfds;
+    //buffers to hold data sent to or received from the local and remote sockets
+    char local_buf[MSS] = {0}, remote_buf[MSS] = {0};
+    //To hold the information of the local server
+    struct sockaddr_in server_name;
+    server_name.sin_family = AF_INET;
+    server_name.sin_port = -1;
+    server_name.sin_addr = LOCALADDRESS;
+    //to keep track of the maximum amount of data the server can accept
+    int ftpsMaxData = -1;
+    //to keep track of the amount of data received
+    int amtRecvd = 0;
+
+    while(1) {
+
+        // clear the set 
+        FD_ZERO(&readfds);
+
+        // add our descriptors to the set
+        FD_SET(local_sock, &readfds);
+        FD_SET(remote_sock, &readfds);
+
+        // Block until input arrives on one or more sockets
+        if(select(FD_SETSIZE, &readfds, NULL, ) < 0) {
+            fprintf(stderr, "%s\n", "There was an issue with select() in tcpd");
+            exit(1);
+        }
+        //Grab data from any socket that is ready
+        if (FD_ISSET(local_sock, &readfds)) {
+            //receive data from the local socket
+            amtRecvd = recvfrom(local_sock, local_buf, sizeof(local_buf), 0, NULL, NULL);
+
+            if ( /*server*/ ) {
+                //get amount of data ftps can accept
+                //ftpsMaxData = ;
+                //get the port number that it's accepting data on
+                //server_name.sin_port = ;
+
+            } else if ( /*client*/ ) { 
+                //forward the data to the remote tcpd through troll
+
+            } else {
+                fprintf(stderr, "%s\n", "Message from unknown source");
+                exit(1);
+            } 
+        } 
+        if (FD_ISSET(remote_sock, &readfds)) {
+            //receive data from the remote socket
+            amtRecvd = recvfrom(remote_sock, remote_buf, sizeof(remote_buf), 0, NULL, NULL);
+
+            //forward data up to ftps
+            if (server_name.sin_port == -1 || ftpsMaxData == -1) {
+                fprintf(stderr, "%s\n", "The server is not ready for packets");
+                exit(1);
+            } else {
+                if (sendto(remote_socket, remote_buf, amtRecvd, 0, (struct sockaddr *)&server_name, sizeof(server_name)) < 0) {
+                    fprintf(stderr, "%s\n", "Error sending to server from local tcpd");
+                    exit(1);
+                }
+            }
+    } // end while
 }
 
